df7329d349675c8b7c28f46c7b6b5cfd11cf4e4e
Jaafar Benabdallah <jaafer.benabdallah@gmail.com>
2016-12-15 11:44:16 -0500

    added getErrror() function

--- firmware/HTU21D.h
+++ firmware/HTU21D.h
@@ -1,8 +1,14 @@
 /*
-Spark Core HTU21D Temperature / Humidity Sensor Library
-By: Romain MP, modified by Jaafar Ben-Abdallah
+Particle Photon HTU21D Temperature / Humidity Sensor Library
+By: Jaafar Ben-Abdallah July 2015, 
+Added: getError() propagates the Wire error id to help troubleshooting a communication problem
+Original development by: Romain MP
 Licence: GPL v3
 */
+
+
+#ifndef HTU21D_h
+#define HTU21D_h
 
 #include "application.h"
 
@@ -14,14 +20,14 @@ Licence: GPL v3
 #define TRIGGER_TEMP_MEASURE_NOHOLD  0xF3
 #define TRIGGER_HUMD_MEASURE_NOHOLD  0xF5
 #define WRITE_USER_REG  0xE6
-#define READ_USER_REG  	0xE7
-#define SOFT_RESET  	0xFE
+#define READ_USER_REG  0xE7
+#define SOFT_RESET  0xFE
 
 // HTU21D Resolution (see datasheet for more)
-#define HTU21D_RES_11BITS 0x81 	//11bits RH & 11bits Temp
-#define HTU21D_RES_MAX 0x00 	//12bits RH & 14bits Temp
+#define HTU21D_RES_11BITS 0x81 //11bits RH & 11bits Temp
+#define HTU21D_RES_MAX 0x00 //12bits RH & 14bits Temp
 
-#define CRC_POLY 0x988000 	// Shifted Polynomial for CRC check
+#define CRC_POLY 0x988000 // Shifted Polynomial for CRC check
 
 // Error codes
 #define HTU21D_I2C_TIMEOUT 	998
@@ -38,11 +44,17 @@ public:
 	float readHumidity();
 	float readTemperature();
 	void setResolution(byte resBits);
-	void reset();
+	bool reset();
+	uint8_t getError(void);
 
 private:
+
+	uint8_t _error;
+
 	//Private Functions
 
 	byte read_user_register();
 	byte checkCRC(uint16_t message_from_sensor, uint8_t check_value_from_sensor);
 };
+
+#endif
